8249a9da4247f52483b42ae78db434b116c63ce4
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-30 17:46:41 -0300

    Finish tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,17 +51,6 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, type: :feature) do
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    unless driver_shares_db_connection_with_specs
-      # Driver is probably for an external browser with an app
-      # under test that does *not* share a database connection with the
-      # specs, so use truncation strategy.
-      DatabaseCleaner.strategy = :truncation
-    end
-  end
-
   config.before(:each) do
     DatabaseCleaner.start
   end
