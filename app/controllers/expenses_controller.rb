a62f978f4d7f8e0dc68e0c4eb2231a8679a234c8
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-30 14:54:58 -0300

    Finish expenses new page

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,22 @@
 class ExpensesController < ApplicationController
   def new
-    
+    @errors = params[:errors]
   end
 
   def create
-
+    @user = current_user
+    @expense = current_user.expenses.new(name: params[:name], amount: params[:amount])
+    if @expense.save
+      params[:categories].each do |category_id|
+        category = Category.find(category_id)
+        @expense.categories << category
+      end
+      flash[:notice] = 'Expense created successfully'
+      redirect_to '/user'
+    else
+      flash[:notice] = 'Something is wrong, please look at the errors'
+      @errors = @category.errors.full_messages
+      redirect_to expenses_new_path(errors: @errors)
+    end
   end
 end
