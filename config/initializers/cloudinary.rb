5cbc89931ed225afd27acd33ae81229f42d3f674
jmontanholi <jonunes9326@hotmail.com.br>
2021-12-01 10:45:46 -0300

    Change cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = "dds4ezlvp"
-  config.api_key = "347877727746449"
-  config.api_secret = "FNRvjEag2PMOjeTSlgvrapjBTxY"
+  config.cloud_name = "hrwa243my"
+  config.api_key = "651168515613976"
+  config.api_secret = "j4ppF_lP4_W4a6H5Ij7S5t3XpUc"
   config.secure = true
 end
